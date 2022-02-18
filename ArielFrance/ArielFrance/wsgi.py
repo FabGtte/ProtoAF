2fd74c4d17d13cafd898c2268588d42e94925155
FabGtte <fabiengilliotte@gmail.com>
2022-02-18 11:00:09 +0100

    Deleted venv activate script - no venv on server

--- ArielFrance/ArielFrance/wsgi.py
+++ ArielFrance/ArielFrance/wsgi.py
@@ -9,9 +9,6 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
-this_file = "venv/bin/activate_this.py"
-exec(open(this_file).read(), {'__file__': this_file})
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ArielFrance.settings')
